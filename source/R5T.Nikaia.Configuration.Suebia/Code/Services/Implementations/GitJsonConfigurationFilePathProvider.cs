0b6c4d11a3467b8e39740074ecf6a09f5b0f3662
MinexAutomation <minex.automation@gmail.com>
2020-07-02 00:09:40 -0700

    Added GitJsonConfigurationFilePathProvider service implementation class type.

---
+++ source/R5T.Nikaia.Configuration.Suebia/Code/Services/Implementations/GitJsonConfigurationFilePathProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Suebia;
+
+
+namespace R5T.Nikaia.Configuration.Suebia
+{
+    public class GitJsonConfigurationFilePathProvider : IGitJsonConfigurationFilePathProvider
+    {
+        private ISecretsDirectoryFilePathProvider SecretsDirectoryFilePathProvider { get; }
+
+
+        public GitJsonConfigurationFilePathProvider(ISecretsDirectoryFilePathProvider secretsDirectoryFilePathProvider)
+        {
+            this.SecretsDirectoryFilePathProvider = secretsDirectoryFilePathProvider;
+        }
+
+        public Task<string> GetGitJsonConfigurationFilePath()
+        {
+            var gitConfigurationJsonFilePath = this.SecretsDirectoryFilePathProvider.GetSecretsFilePath(FileNames.GitConfigurationJsonFileName);
+
+            return Task.FromResult(gitConfigurationJsonFilePath);
+        }
+    }
+}
